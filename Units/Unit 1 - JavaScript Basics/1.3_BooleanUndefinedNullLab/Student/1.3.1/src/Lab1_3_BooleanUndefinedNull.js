a5db6543850af5d35de1883c3dc0e0ddc84e8507
nmazorra26284-eng <nmazorra26284@columbushs.com>
2025-11-10 09:20:13 -0500

    b

--- Units/Unit 1 - JavaScript Basics/1.3_BooleanUndefinedNullLab/Student/1.3.1/src/Lab1_3_BooleanUndefinedNull.js
+++ Units/Unit 1 - JavaScript Basics/1.3_BooleanUndefinedNullLab/Student/1.3.1/src/Lab1_3_BooleanUndefinedNull.js
@@ -5,23 +5,45 @@
 // TODO 1: Booleans
 // - Create isStudent = true, hasLicense = false
 // - Print results of true || false, true && false, !true
-
+let isStudent = true;
+let hasLicense = false;
+console.log(true || false);
+console.log(true && false);
+console.log(!true);
 // TODO 2: Short-circuit evaluation
 // - Print result of false && (5 > 2)
 // - Print result of true || (2 > 5)
 
+console.log(true && (5 > 2));
+console.log(true || (2 > 5));
+
 // TODO 3: Undefined
 // - Declare variable grade and print it
+let grade;
+console.log(grade);
 
 // TODO 4: Null
 // - Create city = null, print it
+let city = null;
+console.log(city);
 // - Reassign city = "Miami" and print again
+city = "Miami"
+console.log(city);
 
 // TODO 5: Immutability
 // - Create word = "hello"
 // - Try word[0] = "H", print word
 // - Reassign word = "Hello" and print again
+let word = "hello"
+word[0] = "H"
+console.log(word);
+word = "Hello";
+console.log(word);
 
 // 🚀 Stretch Goals
 // - Check if number is between 10 and 20 using &&
+let number = 15;
+console.log(number > 10 && number < 20);
+
 // - Check if city is "Miami" or "Berlin" using ||
+console.log(city === "Miami" || city === "Berlin");
--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.1/src/Lab1_4_ConditionalsLoops.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.1/src/Lab1_4_ConditionalsLoops.js
@@ -3,11 +3,59 @@
 */
 
 // TODO 1: if..else with age discounts
+let age = 15;
+if(age<= 16) {
+    console.log("Teen Discount");
+} else if(age>= 65) {
+    console.log("Senior Discount")
+} else {
+    console.log("No Discount")
+}
+    
+let weight = 180;
+if(weight <= 150) {
+    console.log("Your slim")
+} else if (weight <= 190) {
+    console.log("You are in shape")
+} else {
+    console.log("You need to start working out")
+}
+
 
 // TODO 2: Nested if with two numbers
+let number1 = 10
+let number2 = 20
+
+if (typeof number1 == 'number' && typeof number2) {
+    if(number1 == number2) {
+    console.log("Numbers are equal")
+    }else if (number1 > number2){
+    console.log("Number 1 is bigger")
+} else {
+    console.log("Number 1 is smaller")
+}
+}
+ 
 
 // TODO 3: Switch statement for language greeting
 
+let language = "French"
+switch(language) {
+    case "Spanish":
+        console.log("Hola, mundo!")
+        break;
+        case "French":
+            console.log("Bonjour tout le monde")
+            break;
+        default:
+            console.log("Hello, world")
+}
+
+let stock = "APPL"
+
+switch (stock){
+
+}
 // TODO 4: While loop (1 to 10)
 
 // TODO 5: Do..while loop (1 to 5)
